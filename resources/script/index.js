073e555dde87315c0793068831c93e50f1bb8428
Renaldas B <renaldasrenis14@gmail.com>
2025-01-30 14:35:08 +0200

    Update index.js

--- resources/script/index.js
+++ resources/script/index.js
@@ -5,38 +5,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     mobileButton.addEventListener('click', () => {
         if (navMenu.style.display === 'none' || !navMenu.style.display) {
-            mobileButtonHeader.style.display = 'block';
-            navMenu.style.display = 'block';
-            mobileButton
-        } else {
-            navMenu.style.display = 'none';
-            mobileButtonHeader.style.display = 'none';
+            mobileButtonHeader.classList.toggle('active');
+            navMenu.classList.toggle('active');
         }
     })
 
-        var slides = document.querySelectorAll('.slide');
-        var btns = document.querySelectorAll('.btn');
-        let currentSlide = 1;
-
-
-        var manualNav = function(manual) {
-            slides.forEach((slide) => {
-                slide.classList.remove('active');
-
-                btns.forEach((btn) => {
-                    btn.classList.remove('active');
-                })
-            })
-
-            slides[manual].classList.add('active');
-            btns[manual].classList.add('active');
+    function checkWidth() {
+        if (window.innerWidth > 880) {
+            mobileButtonHeader.classList.remove('active');
+            navMenu.classList.remove('active');
         }
+    }
 
-        btns.forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                manualNav(i);
-                currentSlide = i;
+    window.addEventListener('resize', checkWidth);
+
+    var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
+
+    var manualNav = function(manual) {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+
+            btns.forEach((btn) => {
+                btn.classList.remove('active');
             })
         })
+
+        slides[manual].classList.add('active');
+        btns[manual].classList.add('active');
+    }
+
+    btns.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            manualNav(i);
+            currentSlide = i;
+        })
+    })
 })
 
